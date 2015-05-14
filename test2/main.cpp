9674bffd8d1ef0abb9e0ab9ebef41e05c1bd2bf0
kazuhiro77 <kazuhironagai@hotmail.com>
2015-05-13 22:11:00 -0600

    stop coding for lunch

--- test2/main.cpp
+++ test2/main.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <glew.h>
+#include <glut.h>
+#include "glext.h"
 
 
-//test program for github
-//test
+//test program for github. creating a simple modern openGL program using glut
 
 int main(int argc, char* argv[])
 {
